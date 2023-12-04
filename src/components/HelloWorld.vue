a8e24013ee22b69bf2066c27699986e12ede3699
Danil Obukhov <obukhov081189@gmail.com>
2023-12-04 11:27:52 +0500

    Сделана начальная страница

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,13 @@
 <template>
+<div class="container">
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <img class="mainImg" src="../assets/main-page.png">
+    <div class="text">
+      <p class="main_title">Каслинское художественное литье из чугуна.</p><br>
+      <p class="sub_title_main">Из собрания Челябинского государственного музея<br>изобразительных искусств</p>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -41,6 +21,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +35,49 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.container{
+  width: 1400px;
+  margin: 0 auto;
+}
+
+.main_title{
+  font-family: Montserat, sans-serif;
+  font-size: 60px;
+  color: #d0a103;
+}
+
+.sub_title_main{
+  font-family: Montserat, sans-serif;
+  font-size: 35px;
+  color: #d0a103;
+}
+
+.hello{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 956px;
+  background-color: lightgray;
+}
+
+.text{
+  width: 600px;
+  height: 700px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+}
+
+.subTitle{
+  margin-right:183px;
+}
+
+.mainImg{
+float: left;
+}
+.main_title{
+
 }
 </style>
